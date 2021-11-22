6f83cc2620695b5866f5a67d3fcfe045c5790804
pavel golovchan <pavel-golovchan@yandex.ru>
2021-11-22 17:27:14 +0300

    + folder photo-blog

---
+++ project-photo-blog/photo-blog/src/test/java/ru/project/photoblog/PhotoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.photoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
