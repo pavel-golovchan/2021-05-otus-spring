bfcc9fe190c3486d2ee5bf25c12b98d89109d96b
pavel golovchan <pavel-golovchan@yandex.ru>
2021-11-25 02:45:58 +0300

    + SecurityConfiguration = configure spring security: login   admin, user, manager

--- project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/PhotoBlogApplication.java
+++ project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/PhotoBlogApplication.java
@@ -2,7 +2,11 @@ package ru.project.photoblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication
 public class PhotoBlogApplication {
 
