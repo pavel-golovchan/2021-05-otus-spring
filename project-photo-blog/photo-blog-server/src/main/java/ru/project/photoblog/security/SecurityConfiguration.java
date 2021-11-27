75f72ec0f379ca8af50dc51ced3323103c5e3dfc
pavel golovchan <pavel-golovchan@yandex.ru>
2021-11-27 16:40:31 +0300

    отключил безопасность

--- project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/security/SecurityConfiguration.java
+++ project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/security/SecurityConfiguration.java
@@ -4,23 +4,25 @@ package ru.project.photoblog.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+//@Profile("test")
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/");
+        //это отключает не только аутентификацию, но и любые средства защиты, такие как XSS
+        web.ignoring().antMatchers("/**"  );
     }
-
+/*  //отключим безопасность на время отладки/разработки основного функционала
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -38,8 +40,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .logout().logoutUrl("/logout");
-    }
-
+   }
+         */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new PasswordEncoder() {
