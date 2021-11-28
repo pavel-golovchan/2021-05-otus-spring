9523ef065d9fbfdaca0cd80552ee703b3310c0fd
pavel golovchan <pavel-golovchan@yandex.ru>
2021-11-28 05:33:26 +0300

    уникальность аннотаций в приложении (см. они в конфиг.классе есть) @EnableWebSecurity
    @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
    
    если не убрать, то задвоение бинов

--- project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/PhotoBlogApplication.java
+++ project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/PhotoBlogApplication.java
@@ -1,30 +1,13 @@
 package ru.project.photoblog;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.boot.Banner;
 
-import java.io.PrintStream;
-
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @SpringBootApplication
 public class PhotoBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication( PhotoBlogApplication.class);
-	/*	app.setBanner(new Banner() {
-			@Override
-			public void printBanner(Environment environment, Class<?>
-					sourceClass, PrintStream out) {
-				out.print( "PhotoBlog"  );
-			}
-		});
-	 */
 		app.run(args);
 	}
 
