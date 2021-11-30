09e6b1f25baeb78ea66a73cd84d90556139b0c17
pavel golovchan <pavel-golovchan@yandex.ru>
2021-11-30 17:48:17 +0300

    Mail отправка. пока через рест контроллер с параметром и  с авторизацией по токену
    отправка почты http://localhost:8888/api/email/sendSimpleEmail?param=aaaaaaaaaa

---
+++ project-photo-blog/photo-blog-server/src/main/java/ru/project/photoblog/mail/MailConstants.java
@@ -0,0 +1,13 @@
+package ru.project.photoblog.mail;
+
+public class MailConstants {
+    // Replace with your email here:
+    public static final String SENDER_EMAIL = "dev2021java@gmail.com";
+
+    // Replace password!!
+    public static final String SENDER_PASSWORD = "pass@dev2021java";
+
+    // And receiver!
+    public static final String DEFAULT_RECEIVER_EMAIL = "dev2021java@gmail.com";
+
+}
